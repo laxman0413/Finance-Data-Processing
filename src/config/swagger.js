0aab1f1ccf957f72aa11b1dabbf4991cd0eebf33
laxman0413 <vennamlaxman0413@gmail.com>
2026-04-02 00:40:21 +0530

    published using gh pages

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -13,7 +13,7 @@ const options = {
     },
     servers: [
       {
-        url: process.env.NODE_ENV === 'production' ? process.env.API_URL || 'https://api.example.com' : 'http://localhost:3000',
+        url: 'https://laxman0413.github.io/Finance-Data-Processing',
         description: process.env.NODE_ENV === 'production' ? 'Production server' : 'Development server',
       },
     ],
